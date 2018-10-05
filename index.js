31cf880e75a0f582fa80bda7a7b8e9fb0830410a
Ashwin Kumar Shanmugasundaram <Ashwinkumar.S@xe04.ey.com>
2018-10-05 10:46:38 +0530

    appid entered

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var restify = require('restify');
+const path = require('path');
 var builder = require('botbuilder');
-
+const ENV_FILE = path.join(__dirname, '.env');
+const env = require('dotenv').config({ path: ENV_FILE });
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
+
+var id=process.env.MicrosoftAppId
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
