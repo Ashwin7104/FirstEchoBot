532943085caf0a5e050942e0a5dbab178dbc57c8
Ashwin Kumar Shanmugasundaram <Ashwinkumar.S@xe04.ey.com>
2018-10-05 12:21:00 +0530

    added blob storage

---
+++ indexq.js
@@ -0,0 +1,26 @@
+var restify = require('restify');
+const path = require('path');
+var builder = require('botbuilder');
+const ENV_FILE = path.join(__dirname, '.env');
+const env = require('dotenv').config({ path: ENV_FILE });
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+var id=process.env.MicrosoftAppId
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
+});
+
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said from bot: %s", session.message.text);
+});
